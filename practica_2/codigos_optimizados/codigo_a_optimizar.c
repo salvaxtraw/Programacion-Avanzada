9099d46f3222b209ab82384e920b0d463584198c
Anna <anavalentinasalram@gmail.com>
2026-03-12 02:21:11 -0600

    v1.3.0 codigo_a_optimizar.c
    La versión 1.2.0 y 1.2.1 tienen múltiples errores considerables de los que no me percate al subirlo a el repositorio. En esta versión se arreglan todos los problemas.

--- practica_2/codigos_optimizados/codigo_a_optimizar.c
+++ practica_2/codigos_optimizados/codigo_a_optimizar.c
@@ -1,5 +1,5 @@
 /*
-Versión: 1.2.1
+Versión: 1.3.0
 Autor: Saldaña Ramirez Cesar Augusto
 Fecha: 2026-03-12
 
@@ -20,7 +20,11 @@ Tiempo de ejecución
 */
 
 /* 
-MOD: v1.2.1 — Se corrigio el autor y la fecha
+MOD: v1.3.0 — Se arreglaron multiples problemas, entre ellos:
+- Comentarios de cambio donde no hubo cambio.
+- Codigo que realmente no funciona, se copio uno incorrecto y no 
+se verifico antes de subirse al repositorio.
+- Se eliminaron 2 if en if(es_primo) para solo agregar uno al inicio.
 */
 
 #include <stdio.h>
@@ -33,25 +37,26 @@ int main() {
     long long suma_primos = 0;
     int primos_pares = 0;
     int primos_impares = 0;
-    for (int m = 2; m <= N; m++) {
+    if (N >= 2) { // Se agrego este if al inicio para descartar el 2.
+        count_primos++;
+        suma_primos += 2;
+        primos_pares = 1;
+    }
+    for (int m = 3; m <= N; m += 2) {
         int es_primo = 1;
-        int d = 2; // Cambio de la version
+        int d = 3; // Aqui estaba el comentario mal puesto.
         while (d * d <= m) {
             if (m % d == 0) {
                 es_primo = 0;
                 break;
             }
-            d += 2;
+            d += 2; // Aqui debia ir el comentario de la v1.2.0 y v1.2.1
         }
-        if (es_primo) {
-            count_primos++;
-            suma_primos += m;
-            if (m % 2 == 0) {
-                primos_pares++;
-            } else {
-                primos_impares++;
-            }
-        }
+    if (es_primo) {
+        count_primos++;
+        suma_primos += m;
+        primos_impares++;
+        } // Se eliminarion 2 if
     }
     clock_t fin = clock();
     double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC * 1000;
@@ -62,3 +67,8 @@ int main() {
     printf("Tiempo de ejecucion (ms): %.3f\n", tiempo);
     return 0;
 }
+
+/*
+El codigo corre en 0.000ms ya que corre aproximadamente en 0.02ms lo que 
+es mas veloz que un tick (En lo que se mide el tiempo en esta version).
+*/
