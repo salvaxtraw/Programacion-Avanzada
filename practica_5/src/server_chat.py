b03b43dccc87a920a973e084b23fe4a18769bc20
Anna <anavalentinasalram@gmail.com>
2026-04-24 01:26:45 -0600

    v1.1.0

--- practica_5/src/server_chat.py
+++ practica_5/src/server_chat.py
@@ -1,7 +1,7 @@
 # server_chat.py
-# Versión: 1.0.0
+# Versión: 1.0.1
 # Autor: Saldaña Ramirez Cesar Augusto
-# Fecha: 2026-04-23
+# Fecha: 2026-04-24
 # Descripción:
 # Debera encender un servidor por el cual otra computadora se pueda conectar usando threading para enviar
 # un mensaje de texto simple. 
@@ -11,8 +11,29 @@ import socket, threading
 Host = '0.0.0.0' # Segun la documentacion consultada, el puerto EN EL HOST debe ser este para evotar errores
 Port = 5000 
 
+# En las diapositivas mostradas en la ultima clase se mostro la estructura basica de un socket server
+# socket - bind - listen - recv - close
+
 def iniciar_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((Host, Port))
-    server.listen(1)
-    print(f"Esperando coneccion con el cliente...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Socket
+    server.bind((Host, Port)) # Bind
+    server.listen(1) # Listen
+    print(f"(Servidor) Esperando coneccion con el cliente...")
+
+    con, addr = server.accept()
+    print(f"(Servidor) Conectado")
+
+    def recv():
+        while True:
+            try:
+                data = con.recv(1024).decode()
+                if not data:
+                    break
+                print(f"\n(Cliente) {data}")
+            except:
+                break
+
+    threading.Thread(target=recv, daemon=True)
+    while True:
+        msg = input()
+        con.send(msg.encode())
